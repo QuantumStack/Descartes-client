6482005ff9c8b0466814dc06e4728490c40ac5eb
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2019-06-11 23:05:00 -0400

    Added category table modal

---
+++ src/components/StudentCategoryRow.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class StudentCategoryRow extends React.PureComponent {
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    activities: PropTypes.bool,
+    polls: PropTypes.bool,
+    weight: PropTypes.number.isRequired,
+    score: PropTypes.number.isRequired,
+    outOf: PropTypes.number.isRequired,
+  }
+
+  static defaultProps = {
+    description: '',
+    activities: false,
+    polls: false,
+  }
+
+  render() {
+    const {
+      name, description, activities, polls, weight, score, outOf,
+    } = this.props;
+    return (
+      <tr>
+        <td>
+          <span>{name}</span>
+          <br />
+          <span className="uk-text-muted">{description}</span>
+        </td>
+        <td>
+          {score}
+        </td>
+        <td>
+          {outOf}
+        </td>
+        <td>
+          {weight}
+          <span>%</span>
+        </td>
+      </tr>
+    );
+  }
+}
+
+export default StudentCategoryRow;
---
+++ src/components/StudentCategoryTable.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import StudentCategoryRow from './StudentCategoryRow';
+
+class StudentCategoryTable extends React.PureComponent {
+  static propTypes = {
+    categories: PropTypes.objectOf(PropTypes.object).isRequired,
+  }
+
+  render() {
+    const { categories } = this.props;
+    const categoriesArr = Object.values(categories);
+    return (
+      <div className="uk-overflow-auto uk-margin-small-top">
+        <table className="uk-table uk-table-small uk-table-divider">
+          <thead>
+            <tr>
+              <th className="uk-table-expand">Name</th>
+              <th>Score</th>
+              <th>Max</th>
+              <th>Weight</th>
+            </tr>
+          </thead>
+          <tbody>
+            {categoriesArr.map(category => <StudentCategoryRow {...category} />)}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default StudentCategoryTable;
