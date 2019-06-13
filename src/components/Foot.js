8a7752144b58c61903e369e63bd03ba8cf6d1859
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2019-06-13 12:37:02 -0400

    Redesigned footer to address #17

--- src/components/Foot.js
+++ src/components/Foot.js
@@ -11,38 +11,32 @@ function Foot() {
       <div className="uk-section uk-section-secondary uk-section-small">
         <div className="uk-container">
           <div className="uk-grid-small uk-flex-middle" data-uk-grid>
-            <div>
-              Copyright &copy; 2019
-            </div>
+            <div>Copyright &copy; 2019</div>
             <div data-uk-scrollspy="target: span, a; cls: uk-animation-scale-down; delay: 100">
-              <span>Built with </span>
+              <span>Built </span>
+              <span>with </span>
               <span data-uk-icon="heart" />
-              <span> in </span>
-              <span>Pittsburgh</span>
               <span> by </span>
               <a href="https://quantumstack.xyz">QuantumStack</a>
             </div>
-            <div className="uk-width-expand uk-visible@s" />
+            <div className="uk-width-expand@s" />
             <div>
               <a className="uk-button uk-button-text">
-                <span data-uk-icon="question" />
-                &nbsp;
-                Support
+                Credits
               </a>
             </div>
             <div>
-              <a className="uk-button uk-button-text" data-uk-toggle="target: #legal-modal">
-                <span data-uk-icon="file-text" />
-                &nbsp;
+              <button type="button" className="uk-button uk-button-text" data-uk-toggle="target: #legal-modal">
                 Legal
-              </a>
+              </button>
             </div>
             <div>
               <a className="uk-button uk-button-text" href="https://github.com/QuantumStack/Descartes">
-                <span data-uk-icon="github" />
-                &nbsp;
                 GitHub
               </a>
+            </div>
+            <div>
+              <h4 className="uk-text-muted">Descartes</h4>
             </div>
           </div>
         </div>
